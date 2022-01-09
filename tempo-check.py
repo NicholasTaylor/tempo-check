af40b1b74dc1da13db9dd4361e9b56e1836bc194
Nicola Taylor <t@ylornico.la>
2022-01-09 15:43:21 -0500

    Finished Python script

--- tempo-check.py
+++ tempo-check.py
@@ -2,6 +2,7 @@ import requests
 
 SNAP_URL = 'https://tempostorm.com/api/snapshots/findOne'
 DECK_URL = 'https://tempostorm.com/api/decks/findOne'
+FIELDNAMES = ['cardQuantity', 'name', 'cost', 'rarity', 'text', 'attack', 'health', 'cardType', 'expansion']
 
 def get_current_time():
     from datetime import datetime
@@ -25,8 +26,7 @@ def get_slug():
     r_json = r.json()
     return r_json['slugs'][0]['slug']
 
-def get_tiers():
-    slug = get_slug()
+def get_tiers(slug):
     json = {
         "filter": {
             "where": { "slug": slug, "snapshotType": "standard" },
@@ -71,6 +71,12 @@ def get_tiers():
     }
     r = requests.get(SNAP_URL, json=json)
     return r.json()['deckTiers']
+
+def get_card(card):
+    card_data = card['card']
+    new_card = dict((key, value) for key, value in card_data.items() if key in FIELDNAMES)
+    new_card['cardQuantity'] = card['cardQuantity']
+    return new_card
 
 def get_deck(slug):
     json = {
@@ -149,12 +155,43 @@ def get_deck(slug):
         }
     }
     r = requests.get(DECK_URL, json=json)
+    r_json = r.json()
+    name = r_json['name']
+    player_class = r_json['playerClass']
+    cards_raw = r_json['cards']
+    cards = []
+    for card in cards_raw:
+        cards.append(get_card(card))
+    return {
+        'name': name,
+        'slug': slug,
+        'playerClass': player_class,
+        'cards': cards
+    }
 
 def get_decks(decks):
+    deck_list = []
     for deck in decks:
         if deck['tier'] == 1:
-            new_deck = {
-                'rank': deck['ranks'][0] 
-            }
+            new_deck = get_deck(deck['deck']['slugs'][0]['slug'])
+            new_deck['rank'] = deck['ranks'][0]
+            deck_list.append(new_deck)
+    return deck_list
 
-print(get_slug())
+def sort_by_rank(e):
+    return e['rank']
+
+def make_csvs():
+    import csv
+    pub_date = get_slug()
+    decks = get_decks(get_tiers(pub_date))
+    decks.sort(key=sort_by_rank)
+    for deck in decks:
+        filename = pub_date +'_' +'Rank' +str(deck['rank']) +'_' + deck['playerClass'] +'_' + deck['slug'] +'.csv'
+        with open(filename, 'w+') as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=FIELDNAMES)
+            writer.writeheader()
+            for card in deck['cards']:
+                writer.writerow(card)
+
+make_csvs()
